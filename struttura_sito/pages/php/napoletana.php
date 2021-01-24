d9dc88fef282a3ec89e9b1e2b4197a5cc48a80bc
Damiano Zanardo <damiano.zanardo@gmail.com>
2021-01-24 11:38:34 +0100

    sistemati link

--- struttura_sito/pages/php/napoletana.php
+++ struttura_sito/pages/php/napoletana.php
@@ -4,8 +4,8 @@ require_once (__DIR__ . "/../../scripts/php/useful_functions.php");
 
 $page = file_get_contents(__DIR__ . "/../html/napoletana.html");
 $header = file_get_contents(__DIR__ . "/../html/components/header.html");
-$current = '<li class="pizza current" id="napoletana"><a href="napoletana">Pizza Napoletana</a></li>';
-$header = str_replace('<li class="pizza" id="napoletana"><a href="napoletana">Pizza Napoletana</a></li>', $current, $header);
+$current = '<li class="current"><a href="napoletana">Pizza Napoletana</a></li>';
+$header = str_replace('<li><a href="napoletana">Pizza Napoletana</a></li>', $current, $header);
 $links = array();
 
 $links = checkSession();
